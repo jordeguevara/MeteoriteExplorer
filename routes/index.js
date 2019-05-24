bfdced2e082db09d72ab3e612da78c59ab572bd1
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-05-23 22:32:42 -0700

    initial ejs for filling table with api data

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+var request = require("request");
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  request("https://data.nasa.gov/resource/gh4g-9sfh.json?$limit=100", function(
+    error,
+    response,
+    body
+  ) {
+    console.log("error:", error); // Print the error if one occurred and handle it
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    // res.send(body);
+    // console.log(typeof body);
+    res.render("index", { response: body, title: "Landing" });
+  });
+});
+
+module.exports = router;
